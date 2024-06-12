965f58f1df1e043946911167a5f554c04a8a43ad
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-06-12 03:34:40 +0300

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+
+function App() {
+
+  return (
+    <MDBContainer fluid className="p-3 my-5 h-custom">
+
+      <MDBRow>
+
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
+        </MDBCol>
+
+        <MDBCol col='4' md='4'>
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+          <div className="d-flex justify-content-between mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default App;
